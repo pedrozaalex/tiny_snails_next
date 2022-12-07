2b4be37c770b4752f0263ecdafc8435752e53865
--global <--global>
2022-12-07 03:36:22 -0300

    💄 Make short link clickable in ShortLinkDisplay

--- src/components/ShortLinkDisplay.tsx
+++ src/components/ShortLinkDisplay.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import { copyToClipboard } from '../utils/copy';
 import { trpc } from '../utils/trpc';
@@ -15,8 +16,6 @@ export const ShortLinkDisplay: FunctionComponent<Props> = ({ snailId }) => {
         isLoading,
     } = trpc.snail.getById.useQuery(snailId);
 
-    const baseUrl = REDIRECT_BASE_URL;
-
     if (isLoading) {
         return <p>loading...</p>;
     }
@@ -29,20 +28,19 @@ export const ShortLinkDisplay: FunctionComponent<Props> = ({ snailId }) => {
         return <p>snail not found</p>;
     }
 
-    const alias = snail.alias;
+    const url = REDIRECT_BASE_URL + snail.alias;
 
     return (
         <div className="flex items-center justify-center gap-2 rounded-lg border-2 border-black bg-base-200 px-4 py-2">
-            <span>
-                {baseUrl}
-                {alias}
-            </span>
+            <Link href={url} target="_blank" rel="noreferrer" className="link">
+                {url.length > 30 ? url.slice(0, 30) + '...' : url}
+            </Link>
 
             <button
                 className="btn-secondary btn ml-auto"
                 type="button"
                 onClick={() => {
-                    void copyToClipboard(`${baseUrl}${alias}`).then(notify);
+                    void copyToClipboard(url).then(notify);
                 }}
             >
                 copy
@@ -51,4 +49,5 @@ export const ShortLinkDisplay: FunctionComponent<Props> = ({ snailId }) => {
     );
 };
 
-const notify = () => toast({ message: 'copied!' });
+const notify = () =>
+    toast({ message: 'copied to clipboard!', type: 'success' });
