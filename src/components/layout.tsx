6da4f618f9bd3f706ca737a7fb345f63e704997f
--global <--global>
2022-12-07 21:29:41 -0300

    💄 Fix UI bug where content was being cut off

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const Layout: FunctionComponent<Props> = ({ children }) => {
     return (
-        <div className="flex h-screen flex-col bg-primary">
+        <div className="flex flex-col bg-primary">
             <Navbar />
 
             <main className="container mx-auto flex max-w-3xl flex-col gap-12 py-12 px-8">
