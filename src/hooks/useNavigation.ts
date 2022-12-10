21d2949d141574cf2ae363b720ff9fccf6bbc484
--global <--global>
2022-12-10 15:35:45 -0300

    ⚡️ Reuse the same useNavigation object on all renders

--- src/hooks/useNavigation.ts
+++ src/hooks/useNavigation.ts
@@ -1,13 +1,16 @@
 import { useRouter } from 'next/router';
+import { useRef } from 'react';
 
 export function useAppNavigation() {
     const router = useRouter();
 
-    return {
+    const ref = useRef({
         navigateTo: {
             home: () => router.push('/'),
             showSnail: (alias: string) => router.push(`/snails/${alias}`),
             mySnails: () => router.push('/snails'),
         },
-    };
+    });
+
+    return ref.current;
 }
