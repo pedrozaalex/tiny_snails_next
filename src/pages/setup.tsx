d3c9ea82eb9d8a60000568fc698932122b9175b8
--global <--global>
2022-12-18 16:46:44 -0300

    ♻️ Make the setup page easier to read by removing unneeded complexity

--- src/pages/setup.tsx
+++ src/pages/setup.tsx
@@ -1,35 +1,23 @@
 import { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Spinner } from '../components/Spinner';
 import { useAppNavigation } from '../hooks/useNavigation';
 import { trpc } from '../utils/trpc';
 
 const SetupPage: NextPage = () => {
-    const [isSetupReady, setIsSetupReady] = useState(false);
-
     const { navigateTo } = useAppNavigation();
     const { error, mutate: sendRequest } =
         trpc.snail.requestVisitorSnailOwnership.useMutation({
-            onSuccess: () => {
-                setIsSetupReady(true);
-            },
+            onSuccess: navigateTo.homepage,
         });
 
-    useEffect(() => {
-        sendRequest();
-
-        const interval = setInterval(() => {
-            if (isSetupReady) {
-                clearInterval(interval);
-                navigateTo.homepage();
-            }
-        }, 1000);
-    }, [isSetupReady, navigateTo, sendRequest]);
+    useEffect(sendRequest, [sendRequest]);
 
     if (error)
         return (
             <>
-                <h1 className="text-2xl font-bold">Something went wrong</h1>
+                <h1 className="text-2xl font-bold">something went wrong</h1>
+
                 <p>{error.message}</p>
             </>
         );
