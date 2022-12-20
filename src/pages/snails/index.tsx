dee30ae1e82c42be7b92ca72f9de0c8731f3ec6a
--global <--global>
2022-12-20 00:07:32 -0300

    ♻️ Small improvements on the "my snails" page

--- src/pages/snails/index.tsx
+++ src/pages/snails/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 import NextHead from 'next/head';
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { ArrowLeftIcon } from '../../components/ArrowLeftIcon';
 import { ArrowRightIcon } from '../../components/ArrowRightIcon';
 import { Table } from '../../components/Table';
@@ -13,10 +13,27 @@ const Head = () => (
     </NextHead>
 );
 
+enum Actions {
+    INCREMENT_PAGE,
+    DECREMENT_PAGE,
+}
+
+function pageReducer(page = 0, action: Actions) {
+    switch (action) {
+        case Actions.INCREMENT_PAGE:
+            return page + 1;
+        case Actions.DECREMENT_PAGE:
+            const newPage = page - 1;
+            return newPage >= 0 ? newPage : 0;
+        default:
+            return page;
+    }
+}
+
 const MySnailsPage: NextPage = () => {
     const { navigateTo } = useAppNavigation();
 
-    const [page, setPage] = useState(0);
+    const [page, dispatch] = useReducer(pageReducer, 0);
 
     const { data, error, isLoading } = trpc.snail.getMine.useQuery({ page });
 
@@ -54,15 +71,15 @@ const MySnailsPage: NextPage = () => {
             <Table
                 loading={isLoading}
                 objects={data?.snails ?? []}
-                properties={['alias', 'clicks', 'url']}
+                properties={['alias', 'url', 'clicks']}
             />
 
             <div className="flex justify-center gap-2">
                 <button
                     type="button"
                     className="btn-accent btn-square btn"
-                    disabled={page === 0}
-                    onClick={() => setPage((prev) => prev - 1)}
+                    disabled={isLoading || page === 0}
+                    onClick={() => dispatch(Actions.DECREMENT_PAGE)}
                 >
                     <ArrowLeftIcon />
                 </button>
@@ -70,8 +87,8 @@ const MySnailsPage: NextPage = () => {
                 <button
                     type="button"
                     className="btn-accent btn-square btn"
-                    disabled={data?.hasNextPage === false}
-                    onClick={() => setPage((prev) => prev + 1)}
+                    disabled={isLoading || data?.hasNextPage === false}
+                    onClick={() => dispatch(Actions.INCREMENT_PAGE)}
                 >
                     <ArrowRightIcon />
                 </button>
