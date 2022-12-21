ea9d3ca7e66a5fab82b567b9b8cb6aa76d81382a
--global <--global>
2022-12-21 12:22:52 -0300

    🐛 Fix Log Out dialog not showing up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export function Navbar() {
     const { status } = useSession();
 
     const [SignOutDialog, openSignOutDialog] = useDialog({
-        title: 'are you sure?',
+        title: 'log out',
         content: (
             <p className="text-center">are you sure you want to log out?</p>
         ),
@@ -19,129 +19,135 @@ export function Navbar() {
     });
 
     return (
-        <header className="navbar mt-8 justify-between border-y-2 border-primary-content bg-base-100 px-4 py-2 text-primary-content">
-            <h1>
-                <Link
-                    href="/"
-                    className="btn-ghost btn gap-2 text-3xl font-extrabold normal-case"
-                >
-                    tiny snails
-                    <SnailIcon />
-                </Link>
-            </h1>
-
-            {/* The desktop navbar, that stays at the top */}
-            <section className="hidden gap-4 sm:flex">
-                <Link href="/snails" className="btn-ghost btn">
-                    your snails
-                </Link>
-
-                {status === 'authenticated' && (
-                    <>
-                        <button
-                            type="button"
-                            className="btn-secondary btn flex-col px-6"
-                            onClick={openSignOutDialog}
-                        >
-                            <p>log out</p>
-                        </button>
-
-                        <SignOutDialog />
-                    </>
-                )}
-
-                {status === 'unauthenticated' && (
-                    <div className="flex flex-col">
-                        <button
-                            type="button"
-                            className="btn-accent btn flex-col px-6"
-                            onClick={() => void signIn()}
-                        >
-                            <p>sign in</p>
-                        </button>
-                        <small>(to save your snails)</small>
-                    </div>
-                )}
-            </section>
-
-            {/* The mobile navbar, that shows up when the screen is small */}
-            <section className="flex sm:hidden">
-                <button
-                    type="button"
-                    className="btn-ghost btn-square btn"
-                    onClick={() => setShowSidebar(true)}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="#000000"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
+        <>
+            <header className="navbar mt-8 justify-between border-y-2 border-primary-content bg-base-100 px-4 py-2 text-primary-content">
+                <h1>
+                    <Link
+                        href="/"
+                        className="btn-ghost btn gap-2 text-3xl font-extrabold normal-case"
                     >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <line x1="4" y1="8" x2="20" y2="8" />
-                        <line x1="4" y1="16" x2="20" y2="16" />
-                    </svg>
-                </button>
+                        tiny snails
+                        <SnailIcon />
+                    </Link>
+                </h1>
 
-                <div
-                    className={`fixed inset-0 z-10 h-full overflow-hidden transition-all ${
-                        showSidebar ? 'w-full' : 'w-0'
-                    }`}
-                    onClick={() => setShowSidebar(false)}
-                >
-                    <div className="flex h-full w-64 flex-col bg-base-100 text-primary-content shadow-2xl">
-                        <div className="flex-none">
-                            <div className="flex h-20 items-center justify-center">
-                                <Link
-                                    href="/"
-                                    className="btn-ghost btn gap-2 text-3xl font-extrabold normal-case"
-                                >
-                                    tiny snails
-                                    <SnailIcon />
-                                </Link>
-                            </div>
+                {/* The desktop navbar, that stays at the top */}
+                <section className="hidden gap-4 sm:flex">
+                    <Link href="/snails" className="btn-ghost btn">
+                        your snails
+                    </Link>
 
-                            <div className="flex flex-col gap-4 px-4 py-2">
-                                <Link href="/snails" className="btn-ghost btn">
-                                    your snails
-                                </Link>
+                    {status === 'authenticated' && (
+                        <>
+                            <button
+                                type="button"
+                                className="btn-secondary btn flex-col px-6"
+                                onClick={openSignOutDialog}
+                            >
+                                <p>log out</p>
+                            </button>
+                        </>
+                    )}
 
-                                {status === 'authenticated' && (
-                                    <>
-                                        <button
-                                            type="button"
-                                            className="btn-secondary btn flex-col px-6"
-                                            onClick={openSignOutDialog}
-                                        >
-                                            <p>log out</p>
-                                        </button>
+                    {status === 'unauthenticated' && (
+                        <div className="flex flex-col">
+                            <button
+                                type="button"
+                                className="btn-accent btn flex-col px-6"
+                                onClick={() => void signIn()}
+                            >
+                                <p>sign in</p>
+                            </button>
+                            <small>(to save your snails)</small>
+                        </div>
+                    )}
+                </section>
 
-                                        <SignOutDialog />
-                                    </>
-                                )}
+                {/* The mobile navbar, that shows up when the screen is small */}
+                <section className="flex sm:hidden">
+                    <button
+                        type="button"
+                        className="btn-ghost btn-square btn"
+                        onClick={() => setShowSidebar(true)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="#000000"
+                            fill="none"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            {/* <path stroke="none" d="M0 0h24v24H0z" fill="none" /> */}
+                            <line x1="4" y1="8" x2="20" y2="8" />
+                            <line x1="4" y1="12" x2="20" y2="12" />
+                            <line x1="4" y1="16" x2="20" y2="16" />
+                        </svg>
+                    </button>
 
-                                {status === 'unauthenticated' && (
-                                    <div className="flex flex-col">
-                                        <button
-                                            type="button"
-                                            className="btn-accent btn flex-col px-6"
-                                            onClick={() => void signIn()}
-                                        >
-                                            <p>sign in</p>
-                                        </button>
-                                        <small>(to save your snails)</small>
-                                    </div>
-                                )}
+                    <div
+                        className={`fixed inset-0 z-10 h-full overflow-hidden transition-all ${
+                            showSidebar ? 'w-full' : 'w-0'
+                        }`}
+                        onClick={() => setShowSidebar(false)}
+                    >
+                        <div className="flex h-full w-64 flex-col bg-base-100 text-primary-content shadow-2xl">
+                            <div className="flex-none">
+                                <div className="flex h-20 items-center justify-center">
+                                    <Link
+                                        href="/"
+                                        className="btn-ghost btn gap-2 text-3xl font-extrabold normal-case"
+                                    >
+                                        tiny snails
+                                        <SnailIcon />
+                                    </Link>
+                                </div>
+
+                                <div className="flex flex-col gap-4 px-4 py-2">
+                                    <Link
+                                        href="/snails"
+                                        className="btn-ghost btn"
+                                    >
+                                        your snails
+                                    </Link>
+
+                                    {status === 'authenticated' && (
+                                        <>
+                                            <button
+                                                type="button"
+                                                className="btn-secondary btn flex-col px-6"
+                                                onClick={openSignOutDialog}
+                                            >
+                                                <p>log out</p>
+                                            </button>
+
+                                            <SignOutDialog />
+                                        </>
+                                    )}
+
+                                    {status === 'unauthenticated' && (
+                                        <div className="flex flex-col">
+                                            <button
+                                                type="button"
+                                                className="btn-accent btn flex-col px-6"
+                                                onClick={() => void signIn()}
+                                            >
+                                                <p>sign in</p>
+                                            </button>
+                                            <small>(to save your snails)</small>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </header>
+                </section>
+            </header>
+
+            <SignOutDialog />
+        </>
     );
 }
