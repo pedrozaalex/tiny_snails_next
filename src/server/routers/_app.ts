4e281970a954e567ce7a16ae584aec38b331b9dd
--global <--global>
2022-12-21 12:23:33 -0300

    ⚰️ Remove unused "clicks" trpc route

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,10 +1,8 @@
 import { router } from '../trpc';
-import { clickRouter } from './click';
 import { snailRouter } from './snail';
 
 export const appRouter = router({
     snail: snailRouter,
-    click: clickRouter,
 });
 
 // export type definition of API
